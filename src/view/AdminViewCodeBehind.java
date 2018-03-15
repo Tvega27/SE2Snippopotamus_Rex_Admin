fb985bb0e58e6e01dca597ddbb6f365bcb25bb0b
Tyler Vega <tvega1@my.westga.edu>
2018-03-14 22:24:18 -0400

    Started initial repository

---
+++ src/view/AdminViewCodeBehind.java
@@ -0,0 +1,47 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+public class AdminViewCodeBehind {
+
+    @FXML
+    private ListView<?> snippetListView;
+
+    @FXML
+    private Button approveSnippetButton;
+
+    @FXML
+    private Button denySnippetButton;
+
+    @FXML
+    private Button loadFlaggedSnippetBtn;
+
+    @FXML
+    private TextArea snippetDetailsTxtArea;
+
+    @FXML
+    void approveSnippetButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void denySnippetButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFlaggedButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateView(MouseEvent event) {
+
+    }
+
+}
