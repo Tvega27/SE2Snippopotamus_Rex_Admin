a1c04a4951e6a4fd2affdfe5d9925c48b7bcf9a7
Weems <inquisitorcorgi@gmail.com>
2018-03-23 13:26:39 -0400

    Changed JSONHandler methods to static, and started filling out the network interface class

--- src/controller/NetworkInterface.java
+++ src/controller/NetworkInterface.java
@@ -1,15 +1,23 @@
 package controller;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Objects;
 
+import model.CodeSnippet;
+import model.JSONHandler;
+
 public class NetworkInterface {
 	private InetAddress serverAddress;
 	private int portNumber;
 	private Socket comSocket;
+	private DataInputStream in;
+	private PrintStream out;
 
 	public NetworkInterface(InetAddress address, int port) {
 		Objects.requireNonNull(address);
@@ -19,11 +27,6 @@ public class NetworkInterface {
 
 		this.serverAddress = address;
 		this.portNumber = port;
-		try {
-			this.comSocket = new Socket(address, port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	public boolean isReachable() throws UnknownHostException, IOException {
@@ -33,4 +36,125 @@ public class NetworkInterface {
 			return false;
 	}
 
-}
+	public String requestServerDump() {
+		try {
+			comSocket = new Socket("localhost", portNumber);
+			out = new PrintStream(comSocket.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(comSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		out.print("DUMP");
+		String toReturn = null;
+		try {
+			toReturn = in.readUTF();
+			comSocket.close();
+		} catch (IOException e) {
+		}
+		return toReturn;
+
+	}
+
+	public boolean requestServerAdd(CodeSnippet toAdd) {
+		try {
+			comSocket = new Socket("localhost", portNumber);
+			out = new PrintStream(comSocket.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(comSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		String verification = null;
+		out.print("ADD" + " " + JSONHandler.jsonStringFromSnippet(toAdd));
+		try {
+			verification = in.readUTF();
+			comSocket.close();
+		} catch (IOException e) {
+		}
+		if (verification == "?") {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	
+	public boolean requestServerRemove(CodeSnippet toRemove) {
+		try {
+			comSocket = new Socket("localhost", portNumber);
+			out = new PrintStream(comSocket.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(comSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		String verification = null;
+		out.print("REMOVE" + " " + JSONHandler.jsonStringFromSnippet(toRemove));
+		try {
+			verification = in.readUTF();
+			comSocket.close();
+		} catch (IOException e) {
+		}
+		if (verification == "?") {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	public boolean requestServerModify(CodeSnippet origonal, CodeSnippet changed) {
+		
+		
+		try {
+			comSocket = new Socket("localhost", portNumber);
+			out = new PrintStream(comSocket.getOutputStream());
+			in = new DataInputStream(new BufferedInputStream(comSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: hostname");
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		String verification = null;
+		out.print("MODIFY" + " " + JSONHandler.jsonStringFromSnippet(origonal)+JSONHandler.jsonStringFromSnippet(changed) );
+		try {
+			verification = in.readUTF();
+			comSocket.close();
+		} catch (IOException e) {
+		}
+		if (verification == "?") {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
--- src/model/JSONHandler.java
+++ src/model/JSONHandler.java
@@ -8,31 +8,31 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-public class JSONHandler {
+public  class JSONHandler {
 	public JSONHandler() {
 
 	}
 
-	public CodeSnippet codeSnippetFromJSON(String json) {
+	public static CodeSnippet codeSnippetFromJSON(String json) {
 		Gson gson = new Gson();
 
 		return gson.fromJson(json, CodeSnippet.class);
 	}
 
-	public String jsonStringFromSnippet(CodeSnippet snippet) {
+	public static String jsonStringFromSnippet(CodeSnippet snippet) {
 		Gson gson = new Gson();
 
 		return gson.toJson(snippet);
 	}
 
-	public String jsonStringFromSnippetList(List<CodeSnippet> snippets) {
+	public static String jsonStringFromSnippetList(List<CodeSnippet> snippets) {
 		Gson gson = new Gson();
 		
 		return gson.toJson(snippets);
 
 	}
 	
-	public List<CodeSnippet> codeListFromJSONString(String json) {
+	public static List<CodeSnippet> codeListFromJSONString(String json) {
 		Gson gson = new Gson();
 
 		Type codeListType = new TypeToken<ArrayList<CodeSnippet>>(){}.getType();
