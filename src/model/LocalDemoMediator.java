72cb1a02b8e8c27793210f4f5863da694278efc1
Weems <inquisitorcorgi@gmail.com>
2018-03-26 17:06:23 -0400

    Added code to throw errors for unsupported operations in the demo mediator, and a private method to populate it with demo snippits

--- src/model/LocalDemoMediator.java
+++ src/model/LocalDemoMediator.java
@@ -6,6 +6,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class LocalDemoMediator implements Mediator {
 
 	private List<CodeSnippet> serverFiles;
@@ -18,6 +21,8 @@ public class LocalDemoMediator implements Mediator {
 	public LocalDemoMediator(List<CodeSnippet> toPopulate) {
 
 		this.serverFiles = Objects.requireNonNull(toPopulate);
+		this.loggedIn = false;
+		this.populate();
 	}
 
 	@Override
@@ -67,6 +72,7 @@ public class LocalDemoMediator implements Mediator {
 	@Override
 	public boolean logInToServer(String username, String password) {
 		if (username == "user" && password == "pass") {
+			this.loggedIn = true;
 			return true;
 		}
 		return false;
@@ -79,7 +85,7 @@ public class LocalDemoMediator implements Mediator {
 
 	@Override
 	public void setServerAddress(InetAddress serverAddress) {
-
+		throw new UnsupportedOperationException("Demo Mediator does not support this opperation");
 	}
 
 	@Override
@@ -89,7 +95,7 @@ public class LocalDemoMediator implements Mediator {
 
 	@Override
 	public void setPortNumber(int portNumber) {
-
+		throw new UnsupportedOperationException("Demo Mediator does not support this opperation");
 	}
 
 	@Override
@@ -99,6 +105,88 @@ public class LocalDemoMediator implements Mediator {
 
 	@Override
 	public void setServerPubKey(String serverPubKey) {
+		throw new UnsupportedOperationException("Demo Mediator does not support this opperation");
+	}
+
+	private void populate() {
+		this.serverFiles.add(new CodeSnippet("Name1", "Description1", "SomeCode1", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag1"));
+				add(new SimpleStringProperty("tag2"));
+				add(new SimpleStringProperty("tag3"));
+			}
+		}, true));
+		
+		this.serverFiles.add(new CodeSnippet("Name2", "Description2", "SomeCode2", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag1"));
+				add(new SimpleStringProperty("tag5"));
+				add(new SimpleStringProperty("tag4"));
+			}
+		}, false));
+		
+		this.serverFiles.add(new CodeSnippet("Name3", "Description3", "SomeCode3", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag6"));
+				add(new SimpleStringProperty("tag2"));
+				add(new SimpleStringProperty("tag5"));
+			}
+		}, true));
+		
+		this.serverFiles.add(new CodeSnippet("Name4", "Description4", "SomeCode4", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag4"));
+				add(new SimpleStringProperty("tag6"));
+				add(new SimpleStringProperty("tag7"));
+			}
+		}, false));
+		
+		this.serverFiles.add(new CodeSnippet("Name5", "Description5", "SomeCode5", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag7"));
+				add(new SimpleStringProperty("tag1"));
+				add(new SimpleStringProperty("tag3"));
+			}
+		}, true));
+		
+		this.serverFiles.add(new CodeSnippet("Name6", "Description6", "SomeCode6", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag1"));
+				add(new SimpleStringProperty("tag2"));
+				add(new SimpleStringProperty("tag3"));
+			}
+		}, false));
+		
+		this.serverFiles.add(new CodeSnippet("Name7", "Description7", "SomeCode7", new ArrayList<StringProperty>() {
+
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new SimpleStringProperty("tag5"));
+				add(new SimpleStringProperty("tag7"));
+				add(new SimpleStringProperty("tag3"));
+			}
+		}, true));
+		
+
 
 	}
 
