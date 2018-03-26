7a865ed59c6d124d7bafbd89bc47bed2bb29c56e
Tyler Vega <tvega1@my.westga.edu>
2018-03-26 13:11:44 -0400

    for pulling

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(Main.class.getResource("/view/AdminView.fxml"));
+			Scene mainScene = new Scene(root);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Snippopotamus Rex Admin");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText(e.getMessage());
+			alert.show();
 		}
 	}
 	
