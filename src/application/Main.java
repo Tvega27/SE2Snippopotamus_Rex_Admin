c0ec8493bbbcc5dfefd0908837cbfc2c7fa49e79
Tyler Vega <tvega1@my.westga.edu>
2018-03-26 13:22:11 -0400

    For Merge

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,13 +2,9 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 
 public class Main extends Application {
@@ -19,10 +15,6 @@ public class Main extends Application {
 			Scene mainScene = new Scene(root);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Snippopotamus Rex Admin");
-			Parent root = FXMLLoader.load(Main.class.getResource("/view/AdminView.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("What The Hell Tyler :)");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
