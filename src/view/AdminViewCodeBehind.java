09fb16a298cdea55c226e7598dcf376a3980b4ca
Tyler Vega <tvega1@my.westga.edu>
2018-03-26 21:35:54 -0400

    Updated the GUI for tagging and details editing

--- src/view/AdminViewCodeBehind.java
+++ src/view/AdminViewCodeBehind.java
@@ -3,11 +3,14 @@ package view;
 import controller.MainViewController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
 import model.CodeSnippet;
 
@@ -40,6 +43,27 @@ public class AdminViewCodeBehind {
     @FXML
     private RadioButton rbtnFlaggedFilter;
     
+    @FXML
+    private ListView<String> tagListView;
+    
+    @FXML
+    private TitledPane tagsTitlePane;
+
+    @FXML
+    private Button btnAddTag;
+
+    @FXML
+    private Button btnRemoveTag;
+
+    @FXML
+    private TitledPane detailsTitlePane;
+
+    @FXML
+    private TextField nameTextField;
+    
+    @FXML
+    private Accordion detailsAccordian;
+    
     private MainViewController controller;
     
     private CodeSnippet selected;
@@ -48,6 +72,7 @@ public class AdminViewCodeBehind {
     private void initialize() {
     	this.controller = new MainViewController();
     	this.selected = null;
+    	this.detailsAccordian.setExpandedPane(tagsTitlePane);
     	this.initializeListView();
     	this.updateView(null);
     }
@@ -69,19 +94,27 @@ public class AdminViewCodeBehind {
 
     @FXML
     private void loadFlaggedButtonClick(ActionEvent event) {
-    	this.snippetListView.setItems(this.controller.getFlaggedData());
-    	this.approveSnippetButton.setDisable(false);
-    	this.denySnippetButton.setDisable(false);
+    	if(this.rbtnFlaggedFilter.isSelected()) {
+    		this.snippetListView.setItems(this.controller.getFlaggedData());
+        	this.approveSnippetButton.setDisable(false);
+        	this.denySnippetButton.setDisable(false);
+    	} else {
+    		this.snippetListView.setItems(this.controller.getObservableList());
+    		this.approveSnippetButton.setDisable(true);
+        	this.denySnippetButton.setDisable(true);
+    	}
     }
 
     @FXML
     private void updateView(MouseEvent event) {
 		if (this.selected != null) {
 			this.lblSnippetName.textProperty().unbindBidirectional(this.selected.getNameProperty());
+			this.nameTextField.textProperty().unbindBidirectional(this.selected.getNameProperty());
 		}
 		this.selected = this.snippetListView.selectionModelProperty().getValue().getSelectedItem();
 		this.lblSnippetName.textProperty().bindBidirectional(this.selected.getNameProperty());
 		this.snippetDetailsTxtArea.textProperty().setValue(this.selected.getCode().getCodeText());
+		this.nameTextField.textProperty().bindBidirectional(this.selected.getNameProperty());
     }
 
 }
