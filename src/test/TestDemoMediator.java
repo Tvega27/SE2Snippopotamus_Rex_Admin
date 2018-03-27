71fd4bc9da0052bb339978849e43842893ed1c8b
Weems <inquisitorcorgi@gmail.com>
2018-03-26 21:25:03 -0400

    Edited Mediator to be more in line with ISP. Edited the DemoMediator, and tests to match

--- src/test/TestDemoMediator.java
+++ src/test/TestDemoMediator.java
@@ -2,78 +2,31 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.net.InetAddress;
+
 
 import org.junit.jupiter.api.Test;
 
+import model.CodeSnippet;
 import model.LocalDemoMediator;
 import model.Mediator;
 
 class TestDemoMediator {
 
+
+
 	@Test
-	void testGetServerPubKeyReturnNull() {
+	void testSnippetAddMediator() {
 		Mediator testMediator = new LocalDemoMediator();
-		assertNull(testMediator.getServerPubKey());
+		int sizeBefore = testMediator.requestServerDump().size();
+		testMediator.requestServerAdd(new CodeSnippet("test","test","test"));
+		assertTrue(sizeBefore<testMediator.requestServerDump().size() );
 	}
 	@Test
-	void testGetServerAddressReturnNull() {
+	void testSnippetRemoveMediator() {
 		Mediator testMediator = new LocalDemoMediator();
-		assertNull(testMediator.getServerAddress());
-	}
-	@Test
-	void testSetServerPubKeyThrowsException() {
-		
-		Mediator testMediator = new LocalDemoMediator();
-		assertThrows(UnsupportedOperationException.class, ()->{
-			testMediator.setServerPubKey("serverPubKey");
-            });
-		}
-	
-	@Test
-	void testSetPortThrowsException() {
-		
-		Mediator testMediator = new LocalDemoMediator();
-		assertThrows(UnsupportedOperationException.class, ()->{
-			testMediator.setPortNumber(0);
-            });
-		}
-	
-	@Test
-	void testSetServerAddressThrowsException() {
-		
-		Mediator testMediator = new LocalDemoMediator();
-		assertThrows(UnsupportedOperationException.class, ()->{
-			testMediator.setServerAddress(InetAddress.getByName("127.0.0.1"));
-            });
-		}
-	
-	@Test
-	void testIsReachableReturnTrue() {
-		Mediator testMediator = new LocalDemoMediator();
-		assertTrue(testMediator.isReachable());
-	}
-	
-	@Test
-	void testCorrectLoginReturnTrue() {
-		Mediator testMediator = new LocalDemoMediator();
-		assertTrue(testMediator.logInToServer("user", "pass"));
-	}
-	@Test
-	void testIncorrectLoginReturnFalse() {
-		Mediator testMediator = new LocalDemoMediator();
-		assertFalse(testMediator.logInToServer("uset", "pass"));
-	}
-	@Test
-	void testisLoggedInReturnFalseIfNotLoggedIn() {
-		LocalDemoMediator testMediator = new LocalDemoMediator();
-		testMediator.logInToServer("uset", "pass");
-		assertFalse(testMediator.isLoggedIn());
-	}
-	@Test
-	void testisLoggedInReturnTrueIfLoggedIn() {
-		LocalDemoMediator testMediator = new LocalDemoMediator();
-		testMediator.logInToServer("user", "pass");
-		assertTrue(testMediator.isLoggedIn());
+		int sizeBefore = testMediator.requestServerDump().size();
+		CodeSnippet toRemove =  testMediator.requestServerDump().get(0);
+		testMediator.requestServerRemove(toRemove);
+		assertTrue(sizeBefore>testMediator.requestServerDump().size() );
 	}
 }
